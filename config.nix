d8572247308afa6186f095a8a44c57e6cb21b599
Aidymouse <aidymouse4891@gmail.com>
2025-04-01 21:49:35 +1300

    [GamerNix] real

--- config.nix
+++ config.nix
@@ -5,48 +5,15 @@
 { config, pkgs, ... }:
 
 {
+
+	nix.settings.experimental-features = ["nix-command" "flakes"];
+
   imports =
     [ # Include the results of the hardware scan.
-      	./hardware-configuration.nix
+      ./hardware-configuration.nix
     ];
 
-	nix = {
-		package = pkgs.nixFlakes;
-		extraOptions = ''
-			experimental-features = nix-command flakes
-		'';
-	};
-
-	# NVIDIA bullshit
-	hardware.opengl = {
-		enable = true;
-		driSupport = true;
-		driSupport32Bit = true;
-	};
-
-	hardware.nvidia = {
-		modesetting.enable = true;
-
-		powerManagement.enable = false;
-		powerManagement.finegrained = false;
-
-		open = false;
-
-		nvidiaSettings = true;
-
-		# Choose driver version here
-		# options: stable, beta, production, vulkan_beta, legacy_470, legacy_390, legacy_340
-		#package = config.boot.kernelPackages.stable;
-	};
-
-	hardware.bluetooth.enable = true;
-	hardware.bluetooth.powerOnBoot = true;
-
-	services.blueman.enable = true;
-
-	programs.zsh.enable = true;
-  
-	# Bootloader.
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -59,6 +26,11 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+
+  # Enable network manager applet
+  # programs.nm-applet.enable = true;
+
+	programs.sway.enable = true;
 
   # Set your time zone.
   time.timeZone = "Pacific/Auckland";
@@ -78,75 +50,76 @@
     LC_TIME = "en_NZ.UTF-8";
   };
 
-	# Wayland and Sway
-	programs.sway.enable = true;
-	
-	sound.enable = true;
-	nixpkgs.config.pulseaudio = true;
-	hardware.pulseaudio.enable = true;
-
   # Enable the X11 windowing system.
-  #services.xserver.enable = true;
+  services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
+  # Enable the LXQT Desktop Environment.
+  #services.xserver.displayManager.lightdm.enable = true;
+  #services.xserver.desktopManager.lxqt.enable = true;
 
   # Configure keymap in X11
-  #services.xserver = {
-  #  layout = "nz";
-  #  xkbVariant = "";
-  #};
-
-	
-
-  # I stole this from somewhere online...
+  services.xserver.xkb = {
+    layout = "nz";
+    variant = "";
+  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  #sound.enable = true;
-  #hardware.pulseaudio.enable = false;
-  #security.rtkit.enable = true;
-  #services.pipewire = {
-    #enable = true;
-    #alsa.enable = true;
-    #alsa.support32Bit = true;
-    #pulse.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-  #};
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+	programs.zsh = {
+		enable = true;
+	};
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.aidan = {
-    isNormalUser = true;
-    description = "Aidan";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-    #  thunderbird
-    ];
-  };
+	users = {
+		defaultUserShell = pkgs.zsh;
+		users.aidan = {
+			shell = pkgs.zsh;
+			isNormalUser = true;
+			description = "aidan";
+			extraGroups = [ "networkmanager" "wheel" ];
+			packages = with pkgs; [
+			#  thunderbird
+			];
+		};
+	};
 
-	#home-manager.users.aidan =
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "aidan";
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-	nixpkgs.config.allowUnfreePredicate = _: true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+	neovim
   #  wget
-    sway
+	waybar
+	wofi
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -168,12 +141,15 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+	environment.loginShellInit = ''
+		sway
+	'';
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "24.11"; # Did you read the comment?
 
 }
--- flake.nix
+++ flake.nix
@@ -1,36 +1,22 @@
 {
-  description = "flake for enabling flakes";
+  description = "A very basic flake";
 
   inputs = {
-	nixpkgs = {
-		url = "github:NixOS/nixpkgs/nixos-unstable";
-	};
-
-	home-manager = {
-		url = "github:nix-community/home-manager/";
-		inputs.nixpkgs.follows = "nixpkgs";
-	};
-
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, home-manager, nixpkgs }: {
-    nixosConfigurations = {
-      flakeEn = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-	extraSpecialArgs = {inherit inputs;};
-        modules = [
-          ./configuration.nix
-        
-	  home-manager.nixosModules.home-manager
-	  {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
+  outputs = { self, nixpkgs, home-manager }: {
 
-	    home-manager.users.aidan = import ./home.nix;
-	  }
+	nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+		modules = [
+			./config.nix
 
-	];
-      };
-    };
+			home-manager.nixosModules.home-manager {
+				home-manager.users.aidan = import ./home.nix;
+			}
+		];
+	};
+
   };
 }
