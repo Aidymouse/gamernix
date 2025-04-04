5c74dd1ffecf651bab5de2237617c03908262785
Aidan <aidan@thewizardscat.co.nz>
2025-04-04 23:23:01 +1300

    [AGS] add devshell maybe?

--- flake.nix
+++ flake.nix
@@ -4,15 +4,28 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+	ags.url = "github:aylur/ags";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ags, ... }@inputs: let
+	system = "x86_64-linux";
+  in {
 
 	#nixpkgs.config.allowUnfree = true;
 
+	# Set up devshells
+	devShells.${system}.agsShell = nixpkgs.legacyPackages.${system}.mkShell {
+		buildInputs = [
+			(ags.packages.${system}.default.override {
+				#extraPackages = [];
+			})
+		];
+	};
+
+	# Main Config
 	nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
 		specialArgs = {
-			pkgs = import nixpkgs { system="x86_64-linux"; config.allowUnfree = true; };
+			pkgs = import nixpkgs { system=system; config.allowUnfree = true; };
 		};
 		modules = [
 			./config.nix
