99a3137d374089c1d7bc60b7f3ea5eb670bf64f8
Aidan <aidan@thewizardscat.co.nz>
2025-04-05 02:28:59 +1300

    [DevShells] test

---
+++ devShells/cpp/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Flake for development with C++";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: let
+	system = "x86_64-linux";
+	pkgs = nixpkgs.legacyPackages.${system};
+  in {
+
+	#nixpkgs.config.allowUnfree = true;
+
+	# Set up devshells
+	devShells.${system}.default = nixpkgs.legacyPackages.${system}.mkShell {
+		buildInputs = with pkgs; [
+			libclang
+			cmake
+			#X11
+		];
+
+		#shellHook = "Running AGS shell";
+	};
+
+	# Main Config
+
+  };
+}
